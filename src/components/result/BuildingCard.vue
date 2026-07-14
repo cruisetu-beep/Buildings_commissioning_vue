2db4b095eb292062b40fd19197deeae61ee4c70b
CX Workbench Dev <dev@cx-workbench.local>
2026-07-14 07:58:09 +0000

    style: 删除建筑卡片底部区域(归档状态、节点覆盖警告),缩短卡片高度

--- src/components/result/BuildingCard.vue
+++ src/components/result/BuildingCard.vue
@@ -7,8 +7,6 @@ import BuildFuncTag from "../common/BuildFuncTag.vue";
 import HitCountBadge from "../common/HitCountBadge.vue";
 import RuleChipsRow from "../common/RuleChipsRow.vue";
 import CategoryStatusChip from "../common/CategoryStatusChip.vue";
-import ArchiveStatusChip from "../common/ArchiveStatusChip.vue";
-import Icon from "../icons/Icon.vue";
 
 const props = defineProps({
   building: { type: Object, required: true },
@@ -41,16 +39,6 @@ const dimmed = computed(() => props.building.category === "无节点" || props.b
         <span class="rules-hint">{{ building.hitRules.length > 0 ? "(排除D01)" : "" }}</span>
       </div>
       <RuleChipsRow :rules="building.hitRules" :max-show="8" />
-    </div>
-
-    <div class="bld-card-foot">
-      <div class="bld-foot-left">
-        <span v-if="building.nodeCoverage !== '完整'" class="node-warn mono" :title="`节点覆盖:${building.nodeCoverage}`">
-          <Icon name="alert" :size="10" stroke="#d97706" />
-          节点{{ building.nodeCoverage }}
-        </span>
-      </div>
-      <ArchiveStatusChip :status="building.status" />
     </div>
   </div>
 </template>
